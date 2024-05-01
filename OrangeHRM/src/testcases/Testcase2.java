b802b6d59d8823c424c308dd6e8097dd6c63cc14
MAXHUB <MAXHUB@DESKTOP-0RQF8FK>
2024-05-01 12:20:35 +0530

    Testcase 2 updated

--- OrangeHRM/src/testcases/Testcase2.java
+++ OrangeHRM/src/testcases/Testcase2.java
@@ -2,9 +2,10 @@ package testcases;
 
 public class Testcase2 {
 
-	public static void main(String[] args) {
-    System.out.println("This is Testcase2");
-
+	public static void main(String[] args)
+	{
+		System.out.println("This is Testcase2");
+		System.out.println("TestCase 2 updated...");
 
 	}
 
